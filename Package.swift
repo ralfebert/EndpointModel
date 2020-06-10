87daca0f078146f499e5ee004ba70f6a2ca4e7ce
Ralf Ebert <info@ralfebert.de>
2020-06-10 11:07:12 +0200

    support older xcode versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "Endpoint", url: "https://github.com/ralfebert/endpoint.git", from: "0.1.0"),
+        .package(url: "https://github.com/ralfebert/endpoint.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
